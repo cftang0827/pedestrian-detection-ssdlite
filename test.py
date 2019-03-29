259a5c63b860a7a800aa89f55883493578df65e6
cftang <cftang@cftang-office-mbp.local>
2019-03-29 16:40:58 +0800

    update test.py

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
-from api import api
 import cv2
+from pedestrian_detection_ssdlite import api
 from matplotlib import pyplot as plt
 
 img = cv2.imread('test_img/example.jpg')
-bbox_list = api.api(img, 0.6)
+bbox_list = api.get_person_bbox(img, thr=0.6)
+print(bbox_list)
 
 for i in bbox_list:
     cv2.rectangle(img, i[0], i[1], (125, 255, 51), thickness=2)
 
-plt.imshow(img[:, :, ::-1])
-plt.show()
+# plt.imshow(img[:, :, ::-1])
+# plt.show()
