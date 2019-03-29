2c9d4181b8c408f8ec2f6052f4b8198e18518262
cftang <cftang@cftang-office-mbp.local>
2019-03-29 16:41:26 +0800

    update test.py and use matplotlib

--- test.py
+++ test.py
@@ -9,5 +9,5 @@ print(bbox_list)
 for i in bbox_list:
     cv2.rectangle(img, i[0], i[1], (125, 255, 51), thickness=2)
 
-# plt.imshow(img[:, :, ::-1])
-# plt.show()
+plt.imshow(img[:, :, ::-1])
+plt.show()
